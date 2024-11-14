4dcc581b524b42c875482d478fff9717a35d6a1c
Alex Chen <laplacedoge@gmail.com>
2024-11-14 18:30:02 +0800

    test: Add unit tests for lexer module to verify lexeme positions

--- test/lexer.c
+++ test/lexer.c
@@ -38,6 +38,9 @@ TEST NameTokens(void) {
     TOK_STR = "__cache__";
     tok_len = strlen(TOK_STR);
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(9UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_Name, tok->tag);
     ASSERT_EQ_FMT(tok_len, FixedBuf_Size(tok->ext.name.str), "%zu");
     ASSERT_MEM_EQ(TOK_STR, FixedBuf_Data(tok->ext.name.str), tok_len);
@@ -46,12 +49,18 @@ TEST NameTokens(void) {
     TOK_STR = "VAR_2";
     tok_len = strlen(TOK_STR);
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(11UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(5UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_Name, tok->tag);
     ASSERT_EQ_FMT(tok_len, FixedBuf_Size(tok->ext.name.str), "%zu");
     ASSERT_MEM_EQ(TOK_STR, FixedBuf_Data(tok->ext.name.str), tok_len);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(17UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(2UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_NumLit, tok->tag);
     ASSERT_EQ_FMT(47UL, tok->ext.num_lit.val, "%zu");
     tok_idx += 1;
@@ -59,6 +68,9 @@ TEST NameTokens(void) {
     TOK_STR = "agent";
     tok_len = strlen(TOK_STR);
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(19UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(5UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_Name, tok->tag);
     ASSERT_EQ_FMT(tok_len, FixedBuf_Size(tok->ext.name.str), "%zu");
     ASSERT_MEM_EQ(TOK_STR, FixedBuf_Data(tok->ext.name.str), tok_len);
@@ -67,6 +79,9 @@ TEST NameTokens(void) {
     TOK_STR = "ak47";
     tok_len = strlen(TOK_STR);
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(25UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(4UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_Name, tok->tag);
     ASSERT_EQ_FMT(tok_len, FixedBuf_Size(tok->ext.name.str), "%zu");
     ASSERT_MEM_EQ(TOK_STR, FixedBuf_Data(tok->ext.name.str), tok_len);
@@ -75,10 +90,19 @@ TEST NameTokens(void) {
     TOK_STR = "api32sucks";
     tok_len = strlen(TOK_STR);
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(30UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(10UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_Name, tok->tag);
     ASSERT_EQ_FMT(tok_len, FixedBuf_Size(tok->ext.name.str), "%zu");
     ASSERT_MEM_EQ(TOK_STR, FixedBuf_Data(tok->ext.name.str), tok_len);
     tok_idx += 1;
+
+    ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(40UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(0UL, Token_Length(tok), "%zu");
+    ASSERT_TOK_TAG_EQ(TokTag_Eof, tok->tag);
 
     TokSeq_Free(seq);
 
@@ -109,32 +133,59 @@ TEST ComparisonOperatorTokens(void) {
     usize tok_idx = 0;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(2UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_Equ, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(4UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_Assign, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(6UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(2UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_Neq, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(9UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_GreaterThan, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(11UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_LessThan, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(13UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(2UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_Gte, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(16UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(2UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_Lte, tok->tag);
     tok_idx += 1;
+
+    ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(19UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(0UL, Token_Length(tok), "%zu");
+    ASSERT_TOK_TAG_EQ(TokTag_Eof, tok->tag);
 
     TokSeq_Free(seq);
 
@@ -165,36 +216,132 @@ TEST AllKindsOfBracketsTokens(void) {
     usize tok_idx = 0;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_LeftParen, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(2UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_RightParen, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(4UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_LeftBracket, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(5UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_RightBracket, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(7UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_LeftBrace, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(8UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_RightBrace, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(10UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_LessThan, tok->tag);
     tok_idx += 1;
 
     ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(11UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
     ASSERT_TOK_TAG_EQ(TokTag_GreaterThan, tok->tag);
     tok_idx += 1;
+
+    ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(13UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(0UL, Token_Length(tok), "%zu");
+    ASSERT_TOK_TAG_EQ(TokTag_Eof, tok->tag);
+
+    TokSeq_Free(seq);
+
+    Lexer_Free(lex);
+
+    PASS();
+}
+
+TEST ScanMultiLineInput(void) {
+    const char * INPUT_STR =
+        "   if age >= 18 { \r\n"
+        "  \n"
+        "  title = \"adult\";  \n"
+        "} \r\n";
+    const usize INPUT_LEN = strlen(INPUT_STR);
+    const usize NUM_TOKS = 10 + 1;
+
+    Lexer * lex = Lexer_New();
+    ASSERT_NEQ(NULL, lex);
+
+    TokSeq * seq;
+    ASSERT(Lexer_Feed(lex, INPUT_STR, INPUT_LEN));
+    ASSERT(Lexer_Finalize(lex, &seq));
+
+    ASSERT_NEQ(NULL, seq);
+    ASSERT_EQ_FMT(NUM_TOKS, TokSeq_Count(seq), "%zu");
+
+    Token * tok;
+
+    const char * TOK_STR;
+    usize tok_len;
+    usize tok_idx = 0;
+
+    ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(3UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(2UL, Token_Length(tok), "%zu");
+    ASSERT_TOK_TAG_EQ(TokTag_If, tok->tag);
+    tok_idx += 4;
+
+    ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(0UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(16UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
+    ASSERT_TOK_TAG_EQ(TokTag_LeftBrace, tok->tag);
+    tok_idx += 1;
+
+    ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(2UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(2UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(5UL, Token_Length(tok), "%zu");
+    ASSERT_TOK_TAG_EQ(TokTag_Name, tok->tag);
+    tok_idx += 4;
+
+    ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(3UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(0UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(1UL, Token_Length(tok), "%zu");
+    ASSERT_TOK_TAG_EQ(TokTag_RightBrace, tok->tag);
+    tok_idx += 1;
+
+    ASSERT_NEQ(NULL, tok = TokSeq_At(seq, tok_idx));
+    ASSERT_EQ_FMT(4UL, Token_Row(tok), "%zu");
+    ASSERT_EQ_FMT(0UL, Token_Column(tok), "%zu");
+    ASSERT_EQ_FMT(0UL, Token_Length(tok), "%zu");
+    ASSERT_TOK_TAG_EQ(TokTag_Eof, tok->tag);
 
     TokSeq_Free(seq);
 
@@ -207,4 +354,5 @@ SUITE(LexerSuite) {
     RUN_TEST(NameTokens);
     RUN_TEST(ComparisonOperatorTokens);
     RUN_TEST(AllKindsOfBracketsTokens);
+    RUN_TEST(ScanMultiLineInput);
 }
